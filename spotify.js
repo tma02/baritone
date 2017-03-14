1ac4e893b354487a84bcbf9c77e06e2c42f3c531
Tianyi Ma <tn.ma@outlook.com>
2017-03-14 17:25:01 -0500

    Version 1.0.7

--- spotify.js
+++ spotify.js
@@ -20,7 +20,7 @@ const DEFAULT_HTTPS_CONFIG = {
   path: '',
   headers: {
     'Origin': 'https://open.spotify.com',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36';
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
   }
 };
 
